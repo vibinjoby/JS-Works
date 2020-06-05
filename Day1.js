93052be910f1c91c025f8c6904c264d35fa5c69b
vibin <vibin2joby@gmail.com>
2020-06-05 11:36:58 -0400

    day 1 changes

--- Day1.js
+++ Day1.js
@@ -1,11 +1,11 @@
 function run() {
   let count = 0;
 
-  [...Array(1000).keys()].forEach(i => {
+  for (i = 1000; i > 0; i--) {
     if (i % 11 === 0 && i > 17) {
       count += i;
     }
-  });
+  }
   console.log(count);
 }
 
